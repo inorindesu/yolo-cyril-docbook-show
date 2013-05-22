33935a9349eb508129ce60d7cfb430b809c7d7ff
Inori Sakura <inorindesu@gmail.com>
2013-05-23 00:22:24 +0800

    Added another translation for zh_tw and use it instead of default one.

--- build.sh
+++ build.sh
@@ -2,8 +2,10 @@
 DOCBOOK_PATH=/usr/share/sgml/docbook/xsl-stylesheets/html/chunk.xsl
 rm -r ./output/
 mkdir -p ./output/
-mkdir -p ./output/en/ ./output/zh_tw/
+mkdir -p ./output/en/ ./output/zh_tw/ ./output/zh_tw_new/
 ln -s ../../en/img ./output/en/img
 ln -s ../../zh_tw/img ./output/zh_tw/img
+ln -s ../../zh_tw/img ./output/zh_tw_new/img
 xsltproc --xinclude --stringparam base.dir ./output/en/html/ $DOCBOOK_PATH ./en/src/book.xml
 xsltproc --xinclude --stringparam base.dir ./output/zh_tw/html/ $DOCBOOK_PATH ./zh_tw/src/book.xml
+xsltproc --xinclude ./translation/customLayer.xsl ./zh_tw/src/book.xml
---
+++ translation/customLayer.xsl
@@ -0,0 +1,9 @@
+<?xml version='1.0'?>
+<xsl:stylesheet  
+    xmlns:xsl="http://www.w3.org/1999/XSL/Transform" version="1.0">
+
+<xsl:import href="/usr/share/sgml/docbook/xsl-stylesheets/html/chunk.xsl"/>
+<xsl:param name="l10n.xml" select="document('./l10n.xml')"/>
+<xsl:param name="base.dir" select="'./output/zh_tw_new/html/'"/>
+
+</xsl:stylesheet>
