01f71073204f040892d01b0efc96b33e3f2597c6
Inori Sakura <inorindesu@gmail.com>
2013-05-23 00:13:37 +0800

    Added zh_tw translation.

--- build.sh
+++ build.sh
@@ -2,6 +2,8 @@
 DOCBOOK_PATH=/usr/share/sgml/docbook/xsl-stylesheets/html/chunk.xsl
 rm -r ./output/
 mkdir -p ./output/
-mkdir -p ./output/en/
+mkdir -p ./output/en/ ./output/zh_tw/
 ln -s ../../en/img ./output/en/img
+ln -s ../../zh_tw/img ./output/zh_tw/img
 xsltproc --xinclude --stringparam base.dir ./output/en/html/ $DOCBOOK_PATH ./en/src/book.xml
+xsltproc --xinclude --stringparam base.dir ./output/zh_tw/html/ $DOCBOOK_PATH ./zh_tw/src/book.xml
